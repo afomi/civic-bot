efcea78824e1b62f036640450c1c93c1209bd732
Ryan Wold <wold@afomi.com>
2016-03-14 22:09:05 -0700

    tidy url

--- slack/helper_text.rb
+++ slack/helper_text.rb
@@ -4,39 +4,41 @@ module Slack
   class HelperText
 
     def self.cfa(command = "")
+      base_api_url = "http://codeforamerica.org/api"
+
       text = ""
 
       # Respond to each command/resource-name
       if command == "organizations"
-        url = "http://codeforamerica.org/api/organizations"
+        url = "#{base_api_url}/organizations"
         organizations = JSON.parse(open(url).read)["objects"]
 
         organizations.collect do |org|
           text << "#{org['name']} - #{org['website']}\n"
         end
       elsif command == "projects"
-        url = "http://codeforamerica.org/api/projects"
+        url = "#{base_api_url}/projects"
         projects = JSON.parse(open(url).read)["objects"]
 
         projects.collect do |project|
           text << "#{project['name']} - #{project['link_url']}\n"
         end
       elsif command == "events"
-        url = "http://codeforamerica.org/api/events"
+        url = "#{base_api_url}/events"
         events = JSON.parse(open(url).read)["objects"]
 
         events.collect do |event|
           text << "#{event['name']} - #{event['event_url']}\n"
         end
       elsif command == "stories"
-        url = "http://codeforamerica.org/api/stories"
+        url = "#{base_api_url}/stories"
         stories = JSON.parse(open(url).read)["objects"]
 
         stories.collect do |story|
           text << "#{story['title']} - #{story['link']}\n"
         end
       elsif command == "issues"
-        url = "http://codeforamerica.org/api/issues"
+        url = "#{base_api_url}/issues"
         issues = JSON.parse(open(url).read)["objects"]
 
         issues.collect do |issue|
